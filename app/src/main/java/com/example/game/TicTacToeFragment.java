e106486f22e9d926b055b7e39b0744914df226ea
304yasaman <y.taleghani@yahoo.com>
2020-08-18 00:51:21 +0430

    BackGround color is added to fragments. Tic Tac Toe Fragment is being Compelited. Method isGameOver is ToDo.

--- app/src/main/java/com/example/game/TicTacToeFragment.java
+++ app/src/main/java/com/example/game/TicTacToeFragment.java
@@ -7,58 +7,76 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TicTacToeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class TicTacToeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageButton[] mImageButtons = new ImageButton[9];
+    private int[] IsPlayed = new int[9];
+    private int playersTurn = 0;
 
     public TicTacToeFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TicTacToeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TicTacToeFragment newInstance(String param1, String param2) {
-        TicTacToeFragment fragment = new TicTacToeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tic_tac_toe, container, false);
+        View view = inflater.inflate(R.layout.fragment_tic_tac_toe,container,false);
+        findView(view);
+        setListers();
+        return view;
+    }
+
+    private void findView(View view){
+
+        mImageButtons = new ImageButton[]{view.findViewById(R.id.btn1), view.findViewById(R.id.btn2) ,
+                                view.findViewById(R.id.btn3), view.findViewById(R.id.btn4) ,
+                                view.findViewById(R.id.btn5), view.findViewById(R.id.btn6) ,
+                                view.findViewById(R.id.btn7), view.findViewById(R.id.btn8) ,
+                                view.findViewById(R.id.btn9)};
+
+    }
+
+    private void setListers(){
+
+        for (int i = 0; i < 9 ; i++) {
+            final int finalI = i;
+            mImageButtons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mImageButtons[finalI].isEnabled()){
+
+                        if (playersTurn%2==0){
+                            mImageButtons[finalI].setBackgroundResource(R.drawable.cirle_icon);
+                        } else {
+                            mImageButtons[finalI].setBackgroundResource(R.drawable.close_icon);
+                        }
+
+                        IsPlayed[finalI] = playersTurn%2;
+                        playersTurn++;
+                        mImageButtons[finalI].setEnabled(false);
+                    }
+                }
+            });
+        }
+
+
+    }
+
+    private boolean isGameOver(){
+        boolean result = false;
+
+        //To do
+
+        return result;
     }
 }
