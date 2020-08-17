82fff3d9c3cfa85a675084611e09b4db2a742f4f
304yasaman <y.taleghani@yahoo.com>
2020-08-17 22:49:53 +0430

    Initial commit

---
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonTicTocToe;
+    private Button mButton4InaRow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViews();
+        setListeners();
+    }
+
+    private void findViews(){
+        mButtonTicTocToe = findViewById(R.id.btn_TicTocToe);
+        mButton4InaRow = findViewById(R.id.btn_4InRow);
+    }
+
+    private void setListeners(){
+
+        mButtonTicTocToe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                Fragment fragment = fragmentManager.findFragmentById(R.id.Container);
+
+                if (fragment == null) {
+                    TicTacToeFragment ticTacToeFragment = new TicTacToeFragment();
+                    fragmentManager.beginTransaction().add(R.id.Container, ticTacToeFragment).
+                            commit();
+                }
+            }
+        });
+
+        mButton4InaRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                Fragment fragment = fragmentManager.findFragmentById(R.id.Container);
+
+                if (fragment == null) {
+                    FourInRowFragment fourInRowFragment = new FourInRowFragment();
+                    fragmentManager.beginTransaction().add(R.id.Container, fourInRowFragment).
+                            commit();
+                }
+            }
+        });
+    }
+}
